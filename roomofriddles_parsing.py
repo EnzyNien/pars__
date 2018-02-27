b77a3985f8d9d2873bc28de2ab081f6e840aa563
EnzyNien <int_21h@mail.ru>
2018-02-27 20:47:00 +0300

    Update roomofriddles_parsing.py

--- roomofriddles_parsing.py
+++ roomofriddles_parsing.py
@@ -2,8 +2,38 @@ from bs4 import BeautifulSoup
 import requests
 import html5lib
 import json
+import re
+from dateutil import parser
 
 class pars_cls():
+	#_iframe_links - linls of quest name
+	#table_link - links of quest type
+	def returnTime(self,time):
+		time = time.get_text().replace('\t','').replace('\n','')
+		time_class = parser.parse(time)
+		return time_class.strftime("%H:%M")
+
+
+
+	def getFreeandBusyDays(self, link):
+		
+		_table_html = requests.get(link)   
+		_table_bs = BeautifulSoup(_table_html.text, "html5lib")
+		_table_gridDays = _table_bs.find('',{'class':'gridDays'})	
+		_table_gridDay = _table_gridDays.find_all('',{'class':self.p_gridDay})
+		returt_dict = {}
+		for column in _table_gridDay:
+			free_time_arr = []
+			busy_time_arr = []
+			day_class = parser.parse(column.find('',{'class':'gridHeaderDate'})['data-content'])
+			day = day_class.strftime("%Y-%m-%y")
+			free_time = column.find_all('',{'class':self.p_gridFree})
+			free_time_arr = [self.returnTime(i) for i in free_time]
+			busy_time = column.find_all('',{'class':self.p_gridBusy})
+			busy_time_arr = [self.returnTime(i) for i in busy_time]
+			returt_dict.update({day:[{'free':free_time_arr},{'busy':busy_time_arr}]})
+		return returt_dict
+
 
 	def parsing(self):
 		html = requests.get(self.glob_url)
@@ -21,6 +51,7 @@ class pars_cls():
 		for link in _iframe_serviceSelect:
 			childrens = link.findChildren('a')
 			_iframe_links.append(self.iframe_url + childrens[0]['href'])
+			
 		#parsing second page 
 		table_link = []
 		for link in _iframe_links:
@@ -30,7 +61,9 @@ class pars_cls():
 			_teams_teamSelect = _second_page_teams.find_all('',{'class':'teamSelect'})
 			for teams in _teams_teamSelect:
 				childrens = teams.findChildren('a')
-				table_link.append({link:self.iframe_url+childrens[0]['href']})
+				_day_table = self.iframe_url+childrens[0]['href']
+				_day_table_dict = self.getFreeandBusyDays(_day_table)
+				table_link.append({'link_data':{link:_day_table},'time_date':_day_table_dict})
 
 		with open('result.txt','w') as res:
 			json.dump(table_link,res)
@@ -38,6 +71,9 @@ class pars_cls():
 	def __init__(self):
 		self.glob_url = 'https://www.roomofriddles.com/book-now'
 		self.iframe_url = 'https://classic.youcanbook.me'
+		self.p_gridDay= re.compile(r'gridDay(.*)')
+		self.p_gridFree= re.compile(r'(.*)gridFree(.*)')
+		self.p_gridBusy= re.compile(r'(.*)gridBusy(.*)')
 		self.parsing()
 
 if __name__ == '__main__':
